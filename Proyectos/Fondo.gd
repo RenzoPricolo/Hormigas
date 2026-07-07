d9ff67ce517c4d61087f6a9400690e7cd50e2f52
RenzoPricolo <renzoyluciano@gmail.com>
2026-07-06 23:54:35 -0300

    Add files via upload
    Actualizacion, las hormigas de distintos hormigueros pueden perseguirse entre si

--- Proyectos/Fondo.gd
+++ Proyectos/Fondo.gd
@@ -1,30 +1,78 @@
 extends Node2D
+const ROCAS_SCENE = preload("res://Roca.tscn")
 
+onready var contenedor_rocas = $Rocas
 #Genera cada la hormiga
+
+
 const HORMIGA_SCENE = preload("res://Hormiga.tscn")
+const ROCA_SCENE = preload("res://Roca.tscn") # Tu constante de las rocas
 
 onready var contenedor_hormigas = $Hormigas
+onready var nav_mesh = $Navigation2D/NavigationPolygonInstance
+
+
+onready var nodo_hormiguero_comun = $Hormiguero
+onready var nodo_hormiguero_rojo = $HormigueroRojo
 
 func _ready():
 	randomize() 
 
-#Texture boton es el boton de madera
-func _on_TextureButton_pressed():
-	
+#Generacion Hormigas
+
+func _on_TextureButtonComun_pressed():
 	var nueva_hormiga = HORMIGA_SCENE.instance()
+	nueva_hormiga.faccion = "COMUN"
 	
-	#Cambiar a que use la posicion del homiguero
-	var pos_x = 220
-	var pos_y = 1120
-	nueva_hormiga.global_position = Vector2(pos_x, pos_y)
+	#Posibilidad de Clases 0.50=50%
+	if randf() < 0.50:
+		nueva_hormiga.clase = "GUERRERA"
+	else:
+		nueva_hormiga.clase = "RECOLECTORA"
 	
+	if nodo_hormiguero_comun:
+		nueva_hormiga.global_position = nodo_hormiguero_comun.global_position
+	else:
+		nueva_hormiga.global_position = Vector2(220, 1120)
+		
 	contenedor_hormigas.add_child(nueva_hormiga)
-	print("Hormiga nacida en: ", nueva_hormiga.global_position)
+	print("Hormiga Común nacida. Clase: ", nueva_hormiga.clase)
 
 
-onready var nav_mesh = $Navigation2D/NavigationPolygonInstance
+func _on_TextureButtonRojo_pressed():
+	var nueva_hormiga = HORMIGA_SCENE.instance()
+	nueva_hormiga.faccion = "ROJA"
+	
+	
+	if randf() < 0.50:
+		nueva_hormiga.clase = "GUERRERA"
+	else:
+		nueva_hormiga.clase = "RECOLECTORA"
+	
+	if nodo_hormiguero_rojo:
+		nueva_hormiga.global_position = nodo_hormiguero_rojo.global_position
+		
+	contenedor_hormigas.add_child(nueva_hormiga)
+	print("Hormiga Roja nacida. Clase: ", nueva_hormiga.clase)
+
+
+#Rocas
+
+
+func _unhandled_input(event):
+	if event is InputEventKey and event.pressed and event.scancode == KEY_SPACE:
+	
+	   var nueva_roca = ROCAS_SCENE.instance()
+	
+	   var pos_x = get_global_mouse_position().x
+	   var pos_y = get_global_mouse_position().y
+	   nueva_roca.global_position = Vector2(pos_x, pos_y)
+	
+	   contenedor_rocas.add_child(nueva_roca)
+	   print("Roca nacida en: ", nueva_roca.global_position)
+	   actualizar_obstaculos_en_mapa()
 
 
 func actualizar_obstaculos_en_mapa():
 	
-	nav_mesh.bake_navigation_polygon()
+	nav_mesh.navpoly.make_polygons_from_outlines()
--- Proyectos/Hormiga.gd
+++ Proyectos/Hormiga.gd
@@ -1,38 +1,47 @@
 extends KinematicBody2D
+class_name HormigaBase
 
-enum Estado { DEAMBULAR, IDLE, IR_A_COMIDA, REGRESAR_AL_HORMIGUERO }
+export (String, "COMUN", "ROJA") var faccion = "COMUN"
+export (String, "RECOLECTORA", "GUERRERA") var clase = "RECOLECTORA"
+
+enum Estado { DEAMBULAR, IDLE, IR_A_COMIDA, REGRESAR_AL_HORMIGUERO, COMBATIR, HUIR, EN_REFUGIO }
 var estado_actual = Estado.DEAMBULAR
 
 export var velocidad = 120
 var direccion = Vector2.ZERO
-
-#Esto controla el movimiento, creo que podria mejorar aun no enitiendo ese Nodo
 var camino : PoolVector2Array = PoolVector2Array() 
 var nodo_navigation : Navigation2D = null
 
 var posicion_comida = Vector2.ZERO
 var nodo_comida_actual = null
 var posicion_hormiguero = Vector2.ZERO
+var objetivo_enemigo: KinematicBody2D = null
 
 onready var timer = $Dembulamiento
 onready var visual_carga = $Fragmento 
+onready var radar = $VisionHormigas
 
 func _ready():
 	randomize()
 	timer.connect("timeout", self, "_on_Dembulamiento_timeout")
+	radar.connect("body_entered", self, "_on_VisionHormigas_body_entered")
 	if visual_carga: visual_carga.visible = false 
 	
 	nodo_navigation = get_tree().get_root().find_node("Navigation2D", true, false) as Navigation2D
-	
-	var hormiguero = get_tree().get_root().find_node("Hormiguero", true, false)
-	if hormiguero: posicion_hormiguero = hormiguero.global_position
-		
+	buscar_hormiguero_propio()
 	elegir_nueva_direccion()
+
+func buscar_hormiguero_propio():
+	var nombre_hormiguero = "Hormiguero" if faccion == "COMUN" else "HormigueroRojo"
+	var hormiguero = get_tree().get_root().find_node(nombre_hormiguero, true, false)
+	if hormiguero: 
+		posicion_hormiguero = hormiguero.global_position
 
 func _physics_process(delta):
 	match estado_actual:
 		Estado.DEAMBULAR:
 			moverse_por_camino()
+			
 		Estado.IDLE:
 			direccion = Vector2.ZERO
 			
@@ -40,76 +49,71 @@ func _physics_process(delta):
 			if not is_instance_valid(nodo_comida_actual):
 				recalcular_a_deambular()
 				return
-			
 			if camino.size() == 0:
 				calcular_camino_hacia(posicion_comida)
-				
 			moverse_por_camino()
-			
-			#Recoleccion de la comida
-			if get_slide_count() > 0:
-				var choque = get_slide_collision(0)
-		
-				if choque.collider == nodo_comida_actual:
-					recolectar_comida()
-					return 
-			
-			if global_position.distance_to(posicion_comida) < 65: 
+			if global_position.distance_to(posicion_comida) < 200: 
 				recolectar_comida()
+
 		Estado.REGRESAR_AL_HORMIGUERO:
 			if camino.size() == 0:
 				calcular_camino_hacia(posicion_hormiguero)
-				
 			moverse_por_camino()
-			
-			#Cuando llega al hormiguero
-			if get_slide_count() > 0:
-				var choque = get_slide_collision(0)
-				if choque.collider.name == "Hormiguero":
-					entregar_comida()
-					return
-			
-			if global_position.distance_to(posicion_hormiguero) < 60:
+			if global_position.distance_to(posicion_hormiguero) < 200:
 				entregar_comida()
+			
+		Estado.COMBATIR:
+			if not is_instance_valid(objetivo_enemigo) or "estado_actual" in objetivo_enemigo and objetivo_enemigo.estado_actual == Estado.EN_REFUGIO:
+				recalcular_a_deambular()
+				return
+			calcular_camino_hacia(objetivo_enemigo.global_position)
+			moverse_por_camino()
+			if global_position.distance_to(objetivo_enemigo.global_position) < 20:
+				atacar_enemigo()
 
-#Mas del Nodo de Navegacion
+		Estado.HUIR:
+			if camino.size() == 0:
+				calcular_camino_hacia(posicion_hormiguero)
+			moverse_por_camino()
+			if global_position.distance_to(posicion_hormiguero) < 55:
+				entrar_al_refugio()
+
+		Estado.EN_REFUGIO:
+			direccion = Vector2.ZERO
+			visible = false
 
 func calcular_camino_hacia(destino: Vector2):
 	if nodo_navigation:
 		camino = nodo_navigation.get_simple_path(global_position, destino)
 
 func moverse_por_camino():
-	
 	if camino.size() == 0:
 		return
-		
 	var punto_objetivo = camino[0]
-	
 	if global_position.distance_to(punto_objetivo) < 15:
 		camino.remove(0)
 		if camino.size() == 0:
 			return
 		punto_objetivo = camino[0] 
-		
 	direccion = (punto_objetivo - global_position).normalized()
 	move_and_slide(direccion * velocidad)
-	
 	if direccion != Vector2.ZERO:
 		rotation = direccion.angle()
 
 func elegir_nueva_direccion():
-	if randf() < 0.2:
+	if randf() < 0.15:
 		estado_actual = Estado.IDLE
 		camino = PoolVector2Array() 
+		timer.start(rand_range(0.5, 1.5))
 	else:
 		estado_actual = Estado.DEAMBULAR
-		#Modificando este rango deambulan por mas o menos trechos
-		var destino_al_azar = global_position + Vector2(rand_range(-600,600), rand_range(-600, 600))
+		var rango_exploracion = 450
+		var destino_al_azar = global_position + Vector2(
+			rand_range(-rango_exploracion, rango_exploracion), 
+			rand_range(-rango_exploracion, rango_exploracion)
+		)
 		calcular_camino_hacia(destino_al_azar)
-		
-	timer.start(rand_range(1.5, 3.5))
-
-#Cambios de estados
+		timer.start(rand_range(3.5, 6.0))
 
 func detectar_comida(nodo_comida):
 	if estado_actual == Estado.DEAMBULAR or estado_actual == Estado.IDLE:
@@ -129,17 +133,50 @@ func recolectar_comida():
 func entregar_comida():
 	if visual_carga: visual_carga.visible = false         
 	camino = PoolVector2Array() 
-	
 	if is_instance_valid(nodo_comida_actual):
 		estado_actual = Estado.IR_A_COMIDA
 	else:
 		recalcular_a_deambular()
 
+func _on_VisionHormigas_body_entered(body):
+	if "faccion" in body and body.faccion != self.faccion:
+		if estado_actual == Estado.COMBATIR or estado_actual == Estado.HUIR or estado_actual == Estado.EN_REFUGIO:
+			return
+		reaccionar_ante_enemigo(body)
+
+func reaccionar_ante_enemigo(enemigo):
+	if clase == "GUERRERA":
+		estado_actual = Estado.COMBATIR
+		objetivo_enemigo = enemigo
+		camino = PoolVector2Array()
+		timer.stop()
+		print(faccion, " GUERRERA: ¡Al ataque!")
+	else:
+		estado_actual = Estado.HUIR
+		camino = PoolVector2Array()
+		timer.stop()
+		if visual_carga: visual_carga.visible = false
+		print(faccion, " ", clase, ": ¡Huyendo al hormiguero!")
+
+func atacar_enemigo():
+	print("¡Pelea intensa!")
+	if is_instance_valid(objetivo_enemigo):
+		objetivo_enemigo.queue_free()
+	recalcular_a_deambular()
+
+func entrar_al_refugio():
+	estado_actual = Estado.EN_REFUGIO
+	timer.start(rand_range(3.0, 5.0)) 
+
+func _on_Dembulamiento_timeout():
+	if estado_actual == Estado.EN_REFUGIO:
+		visible = true
+		global_position = posicion_hormiguero
+		recalcular_a_deambular()
+	elif estado_actual == Estado.DEAMBULAR or estado_actual == Estado.IDLE:
+		elegir_nueva_direccion()
+
 func recalcular_a_deambular():
 	estado_actual = Estado.DEAMBULAR
 	camino = PoolVector2Array()
 	elegir_nueva_direccion()
-
-func _on_Dembulamiento_timeout():
-	if estado_actual == Estado.DEAMBULAR or estado_actual == Estado.IDLE:
-		elegir_nueva_direccion()
